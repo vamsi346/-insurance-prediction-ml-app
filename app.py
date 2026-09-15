de1f038e5d04a5aa9eeb1b74c5d5abd5da147a4a
vamsi346 <vamsikrishnapalli343@gmail.com>
2026-09-15 15:10:18 +0530

    Refactor app.py for clarity and functionality
    Refactor comments for consistency and clarity, update variable names, and fix minor syntax errors. Adjust user input handling and prediction logic for better functionality.

--- app.py
+++ app.py
@@ -1,96 +1,84 @@
-# step 1 :load import modules
+# Step 1: Load Important Modules
 import pandas as pd
 import numpy as np
-import seaborn as sns 
+import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report,confusion_matrix 
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import accuracy_score
 import streamlit as st
-# this stream lit is for web based application project
+# this streamlit is for web based application project
 
 
-
-
-# web page code
+# Web Page Code
 st.title("HEALTH INSURANCE PREDICTION")
-img_url="https://cdn.zeebiz.com/sites/default/files/2026/03/09/401943-health-insurance.png"
+img_url = "https://cdn.zeebiz.com/sites/default/files/2026/03/09/401943-health-insurance.png"
 st.image(img_url)
 
+# LOAD DATA and ML MODEL PART
 
-# LOAD DATA and ML MODEL PART 
-# step 2 : load insurance data
-url="https://raw.githubusercontent.com/ankitmisk/UIT-data/refs/heads/main/Insurance.csv"
+# Step 2: load Insurance data
+url = "https://raw.githubusercontent.com/ankitmisk/UIT-data/refs/heads/main/Insurance.csv"
 df = pd.read_csv(url)
 
+# Step 3: EDA: Exploratory Data Analysis
+df.drop("Customer_ID", axis = 1, inplace = True)
+
+df['Previous_Insurance'] = df['Previous_Insurance'].map({'No':0,"Yes":1})
+df['Insurance_Bought'] = df['Insurance_Bought'].map({'No':0,"Yes":1})
 
 
-# step 3 EDA: Exploratory Data Analysis
-df.drop ("Customer_ID", axis =1 ,inplace =True)
-
-df['Previous_Insurance']= df['Previous_Insurance'].map( {'No':0,"Yes":1} )
-df['Insurance_Bought'] = df['Insurance_Bought'].map( {'No':0,"Yes":1} )
-
-
-
-# step 4 : divide dataset into features and target
+# Step 4: Divide dataset into features and target
 X = df.iloc[:,:-1]
-Y = df.iloc [:,-1]
+y = df.iloc[:,-1]
 
-
-
-# step 5 : divide data into training & testing part
+# Step 5: Divide data into Training & testing part
 from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.3,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=42, test_size=0.3)
 
-
-
-
-# step 6: train model
+# Step  6: Train Model
 model = LogisticRegression()
-model.fit(X_train,Y_train)
-
+model.fit(X_train,y_train)
 
 
 
 # show data sample
 st.write(df.head())
-
-
-# create sidebar for user input for m
+# Create Side bar for user input form
 st.sidebar.title("Fill Customer Details")
 st.sidebar.image(img_url)
 
 
+# To get user input
 all_ans = []
-for index,col_name in enumerate(X.columns):
-  min_v= X[col_name].min()
-  max_v= X[cil_name].max()
-  if col_name ! = "previous_Insurance":
-    value= st.sidebar.slider(f"select value for {col_name}",
+for index, col_name in enumerate(X.columns):
+  min_v = X[col_name].min()
+  max_v = X[col_name].max()
+  if col_name != "Previous_Insurance":
+    value = st.sidebar.slider(f"Select value for {col_name}",
                              min_value = min_v,
                              max_value = max_v)
   else:
-    value= st.sidebar.number_input(f"select value for {col_name}:")
+    value = st.sidebar.number_input(f"Select value for {col_name} (0:No, 1: yes): ")
 
-all_ans.append(value)
-ud={j:all_ans[i] for i,j in enumerate(x.columns)}
-user_df = pd.DataFrame(ud,index=[1])
-st.weite(user_df)
+  all_ans.append(value)
 
+ud = {j:all_ans[i] for i,j in enumerate(X.columns)}
+user_df = pd.DataFrame(ud, index = [1])
+st.write(user_df)
 
-#============================Prediction===============
-if st.button("Click to predict:"):
-with st spinner("predecting.."):
-             import time 
-time.sleep(2)
-final_ans= model.predection([all_ans])[0]
-if final_ans == 0:
-  st.info("❎customer will not buy the insurance❎")
-else:
-  st.success("✅ customer will buy the insurance✅ ")
-             
-
+#=========================Prediction==============
+if st.button("Click to Predict: "):
+  with st.spinner("Predicting.."):
+    import time
+    time.sleep(2)
+  final_ans = model.predict([all_ans])[0]
+  if final_ans == 0:
+    st.info("❎Customer will not Buy the Insurance❎")
+  else:
+    st.success("✅Customer will buy the Insurance✅")
 
 
+# changes 1: dict
+# changes 2: model predict
